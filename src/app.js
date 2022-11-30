c16d0edf7f86dbd8eb1323af015e84516aee23f7
Sergio Andrés Castellanos Landazábal <serancaslan@gmail.com>
2022-11-30 18:28:10 -0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const app = express();
+
+let root = {
+    type: "dir",
+    children: {
+        home: {
+            type: "dir",
+            children: {
+                myname: {
+                    type: "dir",
+                    children: {
+                        "filea.txt": {
+                            type: "file",
+                        },
+                        "fileb.txt": {
+                            type: "file",
+                        },
+                        "projects": {
+                            type: "dir",
+                            children: {
+                                mysupersecretproject: {
+                                    type: "dir",
+                                    children: {
+                                        mysupersecretfile: {
+                                           type: "file",
+                                        },
+                                    },
+                                }
+                            },
+                        },
+                    }
+                },
+            },
+        }
+    },
+};
+
+const getChildren = (fileName, parentFile, parentName) => {
+    if(fileName === parentName || fileName === ''){
+        if(parentFile.children !== undefined)
+            return Object.keys(parentFile.children)
+        return 'is a file'
+    }
+    if(parentFile.type === 'dir'){
+        let childrenName = Object.keys(parentFile.children)
+        for(let i=0; i<childrenName.length;i++){
+            let children = getChildren(fileName, parentFile.children[childrenName[i]], childrenName[i]);
+            if(children !== 'not found'){
+                return children
+            }
+        }
+        return 'not found'
+    }
+    return 'not found'
+}
+
+app.use(express.json());
+app.get('/*', (req, res)=>{
+    let files = getChildren(req.params[0], root, 'root')
+    res.send({response: files});
+});
+
+module.exports = app
