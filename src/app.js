27e06fe5644e8cefdded779b1b2478c1d41a0ac3
Sergio Andrés Castellanos Landazábal <serancaslan@gmail.com>
2022-12-01 13:49:16 -0500

    cors edit 3

--- src/app.js
+++ src/app.js
@@ -56,7 +56,11 @@ const getChildren = (fileName, parentFile, parentName) => {
     return 'not found'
 }
 
-app.use(cors());
+//app.use(cors());
+app.use((req, res, next) => {
+    res.header({"Access-Control-Allow-Origin": "*"});
+    next();
+}) 
 
 app.use(express.json());
 app.get('/*', (req, res)=>{
